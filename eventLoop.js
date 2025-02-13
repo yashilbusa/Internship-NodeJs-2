12551425525e1e90fd3ad68ae779ad7cda8bd834
yashilbusa <yashilpatel1914@gmail.com>
2025-02-13 13:12:43 +0530

    c

--- eventLoop.js
+++ eventLoop.js
@@ -1,7 +1,19 @@
-console.log("1st");
+// console.log("1st");
 
-setTimeout(function() {
-    console.log("2nd");
-}, 2000);
+// const { resolve } = require("path");
 
-console.log("3rd");
+// setTimeout(()=> {
+//     console.log("2nd");
+// }, 2000);
+
+// console.log("3rd");
+
+
+// setInterval(() => {
+//     console.log("Hello Yashil Busa");
+// }, 1000);
+
+new Promise((resolve,reject)=>{
+    // resolve("Good");
+    reject("Error");
+}).then((msg)=>console.log(msg)).catch((err)=>console.log(err));
