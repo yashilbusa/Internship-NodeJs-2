48303aab40bc24fead37b6e57ea0c52e3b999d3b
yashilbusa <yashilpatel1914@gmail.com>
2025-02-13 18:30:13 +0530

    commit

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ http.createServer((req,res)=>{
     if(req.url==="/"){
         res.writeHead(200,{'content-type': 'text/html'});
         res.end("Home Page");
-        debugger
     }
     else if(req.url==="/about"){
         res.writeHead(200,{'content-type': 'text/html'});       
@@ -22,3 +21,15 @@ http.createServer((req,res)=>{
 }).listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
+
+
+/* 
+
+Today I learned event loop and also revise the callbacks,promises and async/await. then i learn 
+to create http server, handle the different routes, and also done the try/catch blocks and now trying to 
+understand how the debugger work in node js and it's  c,n,s,o and pause methods with command line. and in tasks i
+learn how all event loop works with process.nextTick(), then create http server and also done different routes 
+and also done last two tasks.
+
+
+*/
